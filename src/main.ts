d3d19683a82e4874e3c06184b677a3e707e08664
BlueSkyFish <blueskyfish@kirchnerei.de>
2016-12-16 09:45:40 +0000

    reorder the import of the less files

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@
  * Quickstart
  */
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule }              from './app.module';
-
+// add this gobal app style at first.
 import './app.less';
 
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule }              from './app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
